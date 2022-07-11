7475e0727ab6f4b5edfb2c305b78480a3fd1abd1
minal1593 <minalsalunkhe15@gmail.com>
2022-07-11 22:15:09 +0530

    Added : Program for Gambler_Uc1

---
+++ src/com/Bridgelabz/GamblingProgram/GamblerUc1.java
@@ -0,0 +1,10 @@
+package com.Bridgelabz.GamblingProgram;
+
+class GamblerUc1
+{
+    public static void main(String args[])
+    {
+        int everydayStake = 100;
+        int everygameBet = 1;
+    }
+}
